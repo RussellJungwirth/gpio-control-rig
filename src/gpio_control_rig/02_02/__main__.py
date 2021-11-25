52e394d96681e7593a39071fd433bd5980127473
Russell Jungwirth <jungwirth@healthgrades.com>
2021-11-25 15:44:34 -0800

    use a class for tracking state

--- src/gpio_control_rig/02_02/__main__.py
+++ src/gpio_control_rig/02_02/__main__.py
@@ -8,22 +8,25 @@ BUTTON_PIN = 12
 LAST_STATE = False
 
 
-def button_handler(channel):
-    global LAST_STATE
-    LAST_STATE = not LAST_STATE
-    if LAST_STATE:
-        print('led turned on >>>')
-        GPIO.output(LED_PIN, GPIO.HIGH)
-    else:
-        print('led turned off >>>')
-        GPIO.output(LED_PIN, GPIO.LOW)
+
+class ButtonState:
+    last_state = False
+    def button_handler(self, channel):
+        self.LAST_STATE = not self.LAST_STATE
+        if self.LAST_STATE:
+            print('led turned on >>>')
+            GPIO.output(LED_PIN, GPIO.HIGH)
+        else:
+            print('led turned off >>>')
+            GPIO.output(LED_PIN, GPIO.LOW)
 
 
 @utils.gpio_wrapper
 def run():
+    state_tracker = ButtonState()
     GPIO.setup(LED_PIN, GPIO.OUT)
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=button_handler, bouncetime=300)
+    GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=state_tracker.button_handler, bouncetime=300)
     while True:
         time.sleep(0.005)
 
