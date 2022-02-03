c0124f4f22d179539a0983ede184a2ea9acce885
Russell Jungwirth <jungwirth@healthgrades.com>
2022-02-02 18:46:55 -0800

    first try at rgb

---
+++ src/gpio_control_rig/05_01/__main__.py
@@ -0,0 +1,41 @@
+import time
+import random
+from .. import utils
+import RPi.GPIO as GPIO # noqa: N814
+
+
+class RgbLed:
+    states = [None, None, None]
+
+    def __init__(self, pins, frequency=2000, initial_value=0):
+        self.pins = pins
+        self.frequency = frequency
+        self.initial_value = initial_value
+
+    def startup(self):
+        for index in range(len(self.states)):
+            self.states[index] = GPIO.PWM(self.pins[index], self.frequency)
+            self.states[index].start(self.initial_value)
+
+    def teardown(self):
+        [state.stop() for state in self.states]
+
+    def set(self, values):
+        for x in range(len(values)):
+            self.states[x].ChangeDutyCycle(values[x])
+
+PAUSE_INTERVAL = 1
+LED_PINS = [11, 12, 13]
+
+
+def run():
+    pwm = RgbLed(pins=LED_PINS)
+    with utils.gpio_wrapper(start_hook=pwm.startup, stop_hook=pwm.teardown):
+        GPIO.setup(LED_PINS, GPIO.OUT)
+        GPIO.output(LED_PINS, GPIO.LOW)
+        while True:
+            pwm.set([random.randint(0, 100) for _ in range(3)])
+            time.sleep(PAUSE_INTERVAL)
+
+if __name__ == '__main__':
+    run()
--- src/gpio_control_rig/utils.py
+++ src/gpio_control_rig/utils.py
@@ -1,4 +1,5 @@
 import random
+from re import L
 import string
 import RPi.GPIO as gpio  # noqa: N813
 import gpiozero
@@ -12,25 +13,35 @@ def random_int(min=0, max=99999999):
     return random.randint(min, max)
 
 
-def gpio_wrapper(func):
+def hook_handler(func):
+    if isinstance(func, function):
+        return func()
+
+
+def gpio_wrapper(func, start_hook=None, stop_hook=None):
     def wrapper():
         gpio.setmode(gpio.BOARD)
         try:
+            hook_handler(start_hook)
             func()
         except KeyboardInterrupt:
             print("keyboard interrupt")
         finally:
+            hook_handler(stop_hook)
             gpio.cleanup()
             print("cleanup called")
     return wrapper
 
 
-def gpiozero_wrapper(func):
+def gpiozero_wrapper(func, start_hook=None, stop_hook=None):
     def wrapper():
         if config.ENVIRONMENT == 'dev':
             gpiozero.Device.pin_factory = gpiozero.pins.mock.MockFactory()
         try:
+            hook_handler(start_hook)
             func()
         except KeyboardInterrupt:
             print("keyboard interrupt")
+        finally:
+            hook_handler(stop_hook)
     return wrapper
