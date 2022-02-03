affab15bcc99aed17f7101fe86be3bc1abb13831
Russell Jungwirth <jungwirth@healthgrades.com>
2022-02-02 17:09:40 -0800

    gpio pin setup

--- src/gpio_control_rig/04_01/__main__.py
+++ src/gpio_control_rig/04_01/__main__.py
@@ -2,10 +2,11 @@ import time
 from .. import utils
 import RPi.GPIO as GPIO # noqa: N814
 
+
 class PwmState:
     state = None
 
-    def __init__(self, pin=12, frequency=500, initial_value=0):
+    def __init__(self, pin, frequency=500, initial_value=0):
         self.state = GPIO.PWM(pin, frequency)
         self.state.start(initial_value)
     
@@ -16,10 +17,13 @@ class PwmState:
         self.state.ChangeDutyCycle(value)
 
 INTERVAL = 0.5
+LED_PIN = 12
 
 @utils.gpio_wrapper
 def run():
-    pwm = PwmState()
+    GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.output(LED_PIN, GPIO.LOW)
+    pwm = PwmState(pin=LED_PIN)
     pwm_states = range(1, 101)
     while True:
         [pwm.set(level) for level in pwm_states]
