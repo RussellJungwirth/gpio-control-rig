93e7324b3f5fd362e4e045f6b8c5ba79f49c33f3
Russell Jungwirth <94912355+RussellJungwirth@users.noreply.github.com>
2023-11-18 14:17:42 -0800

    config level caps

--- gpio_control_rig/config.py
+++ gpio_control_rig/config.py
@@ -6,6 +6,6 @@ def str_to_bool(value):
     return str(value).lower() not in ("", "f", "false", "n", "no", "0")
 
 
-LOG_LEVEL = 'info'
+LOG_LEVEL = 'INFO'
 DEBUG_FLAG = str_to_bool(os.getenv('DEBUG_FLAG', '0'))
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'prod').lower()
